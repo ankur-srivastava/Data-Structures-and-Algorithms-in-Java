975208ae254f75ea398bd1e62256010b4a5b3ef4
Ankur Srivastava <ankurlkw@gmail.com>
2018-04-30 14:25:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,12 @@
-class Sample{
-	public static void main(String[] a){
-		System.out.println("Hello World");
-	}
-}
-
-//A simple interface
-
 public interface Sample { 
 	private void printMyStuff(String abc){ 
 		System.out.println(abc); 
+	}
+}
+
+class Sample{
+	public static void main(String[] a){
+		System.out.println("Hello World");
 	}
 }
 
